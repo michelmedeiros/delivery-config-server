32a1732a8278344e21d175672ba6cc851fd0a69f
Michel Medeiros <michelf@ciandt.com>
2018-08-06 17:45:36 -0300

    First commit

---
+++ src/main/java/br/com/coderef/deliveryconfigserver/DeliveryConfigServerApplication.java
@@ -0,0 +1,12 @@
+package br.com.coderef.deliveryconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryConfigServerApplication.class, args);
+	}
+}
